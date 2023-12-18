9a4d8748625ec89f244e6c7a9c0230db919e6d9b
Oscar Theelen <oscartheelen@gmail.com>
2023-12-18 11:45:43 +0100

    first step to making full exe

--- srm/__init__.py
+++ srm/__init__.py
@@ -1,4 +1,46 @@
 from flask import Flask
+from threading import Thread
+from werkzeug.serving import make_server, BaseWSGIServer
+from .Core.Routes import core
+from .Site.Routes import site
+import webview
+import time
+
+class ServerThread(Thread):
+    def __init__(self, app):
+        Thread.__init__(self)
+        self.srv = make_server('127.0.0.1', 5000, app)
+        self.ctx = app.app_context()
+        self.ctx.push()
+
+    def run(self):
+        print('starting server')
+        self.srv.serve_forever()
+
+    def shutdown(self):
+        self.srv.shutdown()
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(core)
+    app.register_blueprint(site)
+    app.config['SERVER_NAME'] = '127.0.0.1:5000'
+
+    server = ServerThread(app)
+    server.start()
+
+    time.sleep(1)  
+
+    webview.create_window('Smart Route Maker', 'http://127.0.0.1:5000')
+    webview.start()
+
+    server.shutdown()
+
+    return app
+
+#old:
+"""
+from flask import Flask
 
 from .Core.Routes import core
 from .Site.Routes import site
@@ -10,3 +52,4 @@ def create_app():
     app.register_blueprint(site)
 
     return app
+"""
